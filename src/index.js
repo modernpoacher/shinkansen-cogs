41b6699d24d076c18459f7a559767413a09abe94
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-06-08 14:36:47 +0100

    `prop-types` `react` `react-dom`

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import CheckboxCog from './cogs/checkbox'
+import EmailCog from './cogs/email'
+import NumberCog from './cogs/number'
+import PasswordCog from './cogs/password'
+import RadioCog from './cogs/radio'
+import SelectCog from './cogs/select'
+import TextCog from './cogs/text'
+import TextareaCog from './cogs/textarea'
+
 export CheckboxCog from './cogs/checkbox'
 export EmailCog from './cogs/email'
 export NumberCog from './cogs/number'
@@ -6,3 +15,14 @@ export RadioCog from './cogs/radio'
 export SelectCog from './cogs/select'
 export TextCog from './cogs/text'
 export TextareaCog from './cogs/textarea'
+
+export default {
+  CheckboxCog,
+  EmailCog,
+  NumberCog,
+  PasswordCog,
+  RadioCog,
+  SelectCog,
+  TextCog,
+  TextareaCog
+}
