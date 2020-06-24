fad95e5278a059305dc0c60ff32082301de2a06a
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-06-24 19:04:07 +0100

    Babel

--- babel.config.js
+++ babel.config.js
@@ -1,40 +1,68 @@
-module.exports = {
-  compact: true,
-  comments: false,
-  presets: [
-    [
-      '@babel/env', {
-        useBuiltIns: 'entry',
-        targets: {
-          node: 'current',
-          browsers: [
-            'last 2 versions'
-          ]
-        },
-        corejs: 3
-      }
-    ],
-    '@babel/react'
+const debug = require('debug')
+
+const log = debug('shinkansen')
+
+const {
+  env: {
+    NODE_ENV = 'development'
+  }
+} = process
+
+log('`shinkansen` is awake')
+
+function env () {
+  log({ NODE_ENV })
+
+  return (
+    NODE_ENV === 'production'
+  )
+}
+
+const presets = [
+  [
+    '@babel/env', {
+      useBuiltIns: 'entry',
+      targets: {
+        node: 'current',
+        browsers: [
+          'last 2 versions'
+        ]
+      },
+      corejs: 3
+    }
   ],
-  plugins: [
-    '@babel/proposal-export-default-from',
-    '@babel/proposal-export-namespace-from',
-    [
-      '@babel/proposal-class-properties',
-      {
-        loose: false
+  '@babel/react'
+]
+
+const plugins = [
+  '@babel/proposal-export-default-from',
+  '@babel/proposal-export-namespace-from',
+  [
+    '@babel/proposal-class-properties',
+    {
+      loose: false
+    }
+  ],
+  [
+    'module-resolver', {
+      root: ['./src'],
+      cwd: 'babelrc',
+      alias: {
+        'shinkansen-cogs': './src',
+        build: './build',
+        stories: './stories'
       }
-    ],
-    [
-      'module-resolver', {
-        root: ['./src'],
-        cwd: 'babelrc',
-        alias: {
-          'shinkansen-cogs': './src',
-          build: './build',
-          stories: './stories'
-        }
-      }
-    ]
+    }
   ]
+]
+
+module.exports = (api) => {
+  if (api) api.cache.using(env)
+
+  return {
+    compact: true,
+    comments: false,
+    presets,
+    plugins
+  }
 }
